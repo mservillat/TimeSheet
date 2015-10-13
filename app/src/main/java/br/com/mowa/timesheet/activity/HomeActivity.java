3a062a5231347f199a9c0367b62cb5287fca198b
walky <walky.java@gmail.com>
2015-10-13 18:03:42 -0300

    segunda 18:00

--- app/src/main/java/br/com/mowa/timesheet/activity/HomeActivity.java
+++ app/src/main/java/br/com/mowa/timesheet/activity/HomeActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -16,13 +18,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import br.com.mowa.timesheet.adapter.RegistrosRecyclerviewAdapter;
 import br.com.mowa.timesheet.dialog.HomeExitDialogFragment;
 import br.com.mowa.timesheet.fragment.NavigationDrawerFragment;
+import br.com.mowa.timesheet.model.TaskModel;
 import br.com.mowa.timesheet.model.UserModel;
 import br.com.mowa.timesheet.network.CallJsonNetwork;
 import br.com.mowa.timesheet.network.VolleySingleton;
+import br.com.mowa.timesheet.parse.ParseTask;
 import br.com.mowa.timesheet.timesheet.R;
 import br.com.mowa.timesheet.utils.SharedPreferencesUtil;
 
@@ -32,6 +39,9 @@ public class HomeActivity extends BaseActivity {
     private DrawerLayout mDrawerLayout;
     private ProgressDialog progress;
     private UserModel user;
+    private ParseTask parseTask;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,16 +52,22 @@ public class HomeActivity extends BaseActivity {
 
 
         this.user = SharedPreferencesUtil.getUserFromSharedPreferences();
-        jsonNetwork = new CallJsonNetwork();
+        this.jsonNetwork = new CallJsonNetwork();
+        this.parseTask = new ParseTask();
 
 
         this.mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_home_drawer_layout);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.activity_home_fragment_navigation_drawer_container);
         drawerFragment.setUp(mDrawerLayout, createToolbar(R.id.activity_home_toolbar));
 
+        this.recyclerView = (RecyclerView) findViewById(R.id.activity_home_recycler);
+        this.layoutManager = new LinearLayoutManager(getActivity());
+        this.recyclerView.setLayoutManager(layoutManager);
+        this.recyclerView.setHasFixedSize(true);
 
-//        this.tvQuantidadeDeHoras = (TextView) findViewById(R.id.activity_home_text_view_horas_semanais);
-//        loadDisplayAllHoursWork();
+        this.tvQuantidadeDeHoras = (TextView) findViewById(R.id.activity_home_text_view_horas_semanais);
+        loadDisplayUltumateTasks();
+        loadDisplayAllHoursWork();
 
 
 
@@ -78,7 +94,13 @@ public class HomeActivity extends BaseActivity {
                             time = time + jsonObject.optLong("time");
                         }
                     }
-                    tvQuantidadeDeHoras.setText(String.format(" %d min ", TimeUnit.MILLISECONDS.toMinutes(time)));
+
+                    String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(time),
+                            TimeUnit.MILLISECONDS.toMinutes(time) % TimeUnit.HOURS.toMinutes(1),
+                            TimeUnit.MILLISECONDS.toSeconds(time) % TimeUnit.MINUTES.toSeconds(1));
+
+                    tvQuantidadeDeHoras.setText(hms);
+//                    tvQuantidadeDeHoras.setText(String.format(" %d min ", TimeUnit.MILLISECONDS.toMinutes(time)));
                     tvQuantidadeDeHoras.refreshDrawableState();
                     progress.dismiss();
 
@@ -93,6 +115,35 @@ public class HomeActivity extends BaseActivity {
             }
         });
     }
+
+
+
+
+    private void loadDisplayUltumateTasks() {
+        jsonNetwork.callJsonObjectGet(VolleySingleton.URL_GET_TASK_USER_ID + this.user.getId(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                List<TaskModel> list = new ArrayList<>();
+                try {
+                    List<TaskModel> listTask = parseTask.jsonObjectToTaskEntity(response);
+                    for(int i = 0; i < 2; i++) {
+                        list.add(listTask.get(i));
+                    }
+                    RegistrosRecyclerviewAdapter adapter = new RegistrosRecyclerviewAdapter(list, null);
+                    recyclerView.setAdapter(adapter);
+                    registerForContextMenu(recyclerView);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+
 
 
     @Override
